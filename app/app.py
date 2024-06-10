e33d1a948889e10a49630b5eda0bbc69ee400e7a
Ivan Todorovic <itodorovic@gmail.com>
2024-06-10 09:57:18 +0200

    new entry

--- app/app.py
+++ app/app.py
@@ -66,3 +66,7 @@ def get_embeddings(embedding_input: EmbeddingInput):
     }
 
     return response
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
